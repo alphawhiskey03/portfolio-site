5183656d16bbb8d556c406bbf973fe70a1db83b7
Vignesh <46638649+alphawhiskey03@users.noreply.github.com>
2021-10-04 20:51:55 +0530

    meta tags

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -14,6 +14,11 @@ export const Layout = ({children}) => {
     <meta name="description" content=" I'm an aspiring and passionate developer, I've been super enthusiastic about learning web/cross-platform technologies, enduring my journey as a full stack web developer. 
 "/> 
     <meta name="keywords" content="web developer,react,express,node,mongodb,php,fullstack,developer"/>
+    <meta name="geo.region" content="IN-TN" />
+    <meta name="geo.placename" content="Coimbatore" />
+    <meta name="geo.position" content="11.057688;76.968131" />
+    <meta name="ICBM" content="11.057688, 76.968131" />
+
       </Head>
      <Header/>
      <main>{children}</main> 
