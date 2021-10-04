1878a53c826f22d1233acd1ed43f1ccf10231857
Vignesh <46638649+alphawhiskey03@users.noreply.github.com>
2021-10-04 20:47:36 +0530

    meta tags

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -10,6 +10,10 @@ export const Layout = ({children}) => {
     <Container>
       <Head>
         <title>Vig | portfolio</title>
+    <meta name="title" content="Vignesh web dev"/>
+    <meta name="description" content=" I'm an aspiring and passionate developer, I've been super enthusiastic about learning web/cross-platform technologies, enduring my journey as a full stack web developer. 
+"/> 
+    <meta name="keywords" content="web developer,react,express,node,mongodb,php,fullstack,developer"/>
       </Head>
      <Header/>
      <main>{children}</main> 
