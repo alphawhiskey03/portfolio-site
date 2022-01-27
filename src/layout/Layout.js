3ac9f6a50388917031cb1137cd6c20c5ff9b6eed
vignesh03 <vignesh.wit2021@gmail.com>
2022-01-27 23:36:14 +0530

    feat(app) : Importing heroText for meta data description

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
-import { Container } from './LayoutStyles'
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import { Container } from "./LayoutStyles";
 import Head from "next/head";
+import { heroText } from "../constants/constants";
 
-export const Layout = ({children}) => {
+export const Layout = ({ children }) => {
   return (
     <Container>
       <Head>
         <title>Vignesh | Software developer coimbatore</title>
-    <meta name="title" content="Vignesh | developer portfolio"/>
-    <meta name="description" content=" I'm an aspiring and passionate developer, I've been super enthusiastic about learning web/cross-platform technologies, enduring my journey as a full stack web developer. 
-"/> 
-    <meta property="og:image" content="/images/vignesh-developer.png" />
-    <meta name="keywords" content="web,react,express,node,mongodb,php,fullstack,developer"/>
-    <meta name="geo.region" content="IN-TN" />
-    <meta name="geo.placename" content="Coimbatore" />
-    <meta name="geo.position" content="11.057688;76.968131" />
-    <meta name="ICBM" content="11.057688, 76.968131" />
-    <meta name="robots" content="index, follow"/>
-
+        <meta name="title" content="Vignesh | developer portfolio" />
+        <meta name="description" content={heroText} />
+        <meta property="og:image" content="/images/vignesh-developer.png" />
+        <meta
+          name="keywords"
+          content="web,react,express,node,mongodb,php,fullstack,developer"
+        />
+        <meta name="geo.region" content="IN-TN" />
+        <meta name="geo.placename" content="Coimbatore" />
+        <meta name="geo.position" content="11.057688;76.968131" />
+        <meta name="ICBM" content="11.057688, 76.968131" />
+        <meta name="robots" content="index, follow" />
       </Head>
-     <Header/>
-     <main>{children}</main> 
-     <Footer/>
+      <Header />
+      <main>{children}</main>
+      <Footer />
     </Container>
-  )
-}
+  );
+};
