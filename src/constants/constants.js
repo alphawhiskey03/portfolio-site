d5707c15ec938545539a8cce6531f618742117ff
vignesh03 <vignesh.wit2021@gmail.com>
2022-01-27 23:35:07 +0530

    feat(app) : New constants for hero and timeline content

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,45 +1,62 @@
 export const projects = [
   {
-    title: 'Rotaract 3201',
-    description: "Got the privilege of working in the official website of rotaract 3201. it's a web portal for the maintenance of the organization and its events . Built it with three level admin architecture and automated the procedural aspects of the organization.",
-      image: '/images/rotaract.png',
-      tags: ['Php', 'MySQL', 'JavaScript'],
-    source: 'https://www.rotaract3201.org/',
-    visit: 'https://www.rotaract3201.org/',
+    title: "Rotaract 3201",
+    description:
+      "Got the privilege of working in the official website of rotaract 3201. it's a web portal for the maintenance of the organization and its events . Built it with three level admin architecture and automated the procedural aspects of the organization.",
+    image: "/images/rotaract.png",
+    tags: ["Php", "MySQL", "JavaScript"],
+    source: "https://www.rotaract3201.org/",
+    visit: "https://www.rotaract3201.org/",
     id: 0,
   },
   {
-    title: 'OneSoil',
-    description:"Worked on a part of OneSoil . A web platform for precision farming. handled the api integration and  terrestrial map plots",
-    image: '/images/onesoil.png',
-    tags: ['MapBox', 'JavaScript'],
-    source: 'https://onesoil.ai/en/',
-    visit: 'https://onesoil.ai/en/',
+    title: "OneSoil",
+    description:
+      "Worked on a part of OneSoil . A web platform for precision farming. handled the api integration and  terrestrial map plots",
+    image: "/images/onesoil.png",
+    tags: ["MapBox", "JavaScript"],
+    source: "https://onesoil.ai/en/",
+    visit: "https://onesoil.ai/en/",
     id: 1,
   },
   {
-    title: 'Scalability Engineers',
-    description: "Blog and career portal for Scalability engineers",
-      image: '/images/scalability.png',
-      tags: ['JavaScript', 'Php','MySQL'],
-    source: 'https://scalabilityengineers.com/',
-    visit: 'https://scalabilityengineers.com/',
+    title: "Scalability Engineers",
+    description:
+      "Blog and career portal for Scalability engineers a leading, well-respected, technology solutions provider. They are a group of database and technology experts.",
+    image: "/images/scalability.png",
+    tags: ["JavaScript", "Php", "MySQL"],
+    source: "https://scalabilityengineers.com/",
+    visit: "https://scalabilityengineers.com/",
     id: 2,
   },
   {
-    title: 'SR7',
-    description: "E-commerce site for SR7.",
-    image: '/images/sr7.png',
-    tags: ['Javascript', 'Php', 'MySQL'],
-    source: 'http://swetharaju7.com/',
-    visit: 'http://swetharaju7.com/',
+    title: "SR7",
+    description:
+      "An e-commerce site for SR7 designers and bridal house. They provide top notch customised outfits and fashion accessories.",
+    image: "/images/sr7.png",
+    tags: ["Javascript", "Php", "MySQL"],
+    source: "http://swetharaju7.com/",
+    visit: "http://swetharaju7.com/",
     id: 3,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2018, text: 'Started my journey', },
-  { year: 2019, text: 'Learnt web development with python and php', },
-  { year: 2020, text: 'Started working as a freelance developer with my first E-commerce', },
-  { year: 2021, text: 'Switched to MERN Stack. Landed on a MERN stack developer job at Sierra digital inc.', },
+  { year: 2018, text: "Started my journey" },
+  { year: 2019, text: "Learnt web development with python and php" },
+  {
+    year: 2020,
+    text: "Started working as a freelance developer with my first E-commerce",
+  },
+  {
+    year: 2021,
+    text: "Switched to MERN Stack. Landed on a MERN stack developer job at Sierra digital inc.",
+  },
 ];
+
+export const heroText =
+  " A passionate software developer, who has been super enthusiastic about learning and working with web/cross-platform technologies.";
+export const timelineText = `${heroText}  Currently
+working as a full stack developer . I have an experience of almost 2
+years, Worked on 5+ realtime projects. I specialize in building
+e-commerce and organistational management systems `;
