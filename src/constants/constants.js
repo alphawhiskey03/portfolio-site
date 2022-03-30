9969fda94810d4a6bd9075ab19f28f2a0b9e3d46
vignesh03 <alphvignesh98@gmail.com>
2022-03-30 11:35:19 +0530

    feat(app): seperate values for freelance and my projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,45 +1,192 @@
 export const projects = [
   {
-    title: 'Rotaract 3201',
-    description: "Got the privilege of working in the official website of rotaract 3201. it's a web portal for the maintenance of the organization and its events . Took care of the the back-end of the website , built it with three level admin architecture and automated the procedural aspects of the organization.",
-      image: '/images/rotaract.png',
-      tags: ['Php', 'MySQL', 'JavaScript'],
-    source: 'https://www.rotaract3201.org/',
-    visit: 'https://www.rotaract3201.org/',
+    title: "Rotaract 3201",
+    description:
+      "Got the privilege of working in the official website of rotaract 3201. it's a web portal for the maintenance of the organization and its events . Took care of the the back-end of the website , built it with three level admin architecture and automated the procedural aspects of the organization.",
+    image: "/images/rotaract.png",
+    tags: [
+      {
+        name: "php",
+        link: "https://www.php.net/",
+      },
+      {
+        name: "mysql",
+        link: "https://www.mysql.com/",
+      },
+      {
+        name: "javascript",
+        link: "https://www.javascript.com/",
+      },
+      {
+        name: "fpdf",
+        link: "http://www.fpdf.org/",
+      },
+      {
+        name: "php Mailer",
+        link: "https://github.com/PHPMailer/PHPMailer",
+      },
+    ],
+    visit: "https://www.rotaract3201.org/",
     id: 0,
   },
   {
-    title: 'OneSoil',
-    description:"Worked on a part of OneSoil . A web platform for precision farming. handled the api integration and  terrestrial map plots",
-    image: '/images/onesoil.png',
-    tags: ['MapBox', 'JavaScript'],
-    source: 'https://onesoil.ai/en/',
-    visit: 'https://onesoil.ai/en/',
+    title: "OneSoil",
+    description:
+      "The project which laid my foundation in javascript. i Worked on a part of OneSoil . A web platform for precision farming. handled the api integration and  terrestrial map plots.",
+    image: "/images/onesoil.png",
+    tags: [
+      { name: "mapbox", link: "https://www.mapbox.com/" },
+      {
+        name: "javascript",
+        link: "https://www.javascript.com/",
+      },
+    ],
+    visit: "https://onesoil.ai/en/",
     id: 1,
   },
   {
-    title: 'Scalabilty Engineers',
-    description: "Blog and career portal for scalability engineers",
-      image: '/images/scalability.png',
-      tags: ['JavaScript', 'Php','MySQL'],
-    source: 'https://scalabilityengineers.com/',
-    visit: 'https://scalabilityengineers.com/',
+    title: "Scalabilty Engineers",
+    description: "Blog and career portal for scalability engineers.",
+    image: "/images/scalability.png",
+    tags: [
+      {
+        name: "php",
+        link: "https://www.php.net/",
+      },
+      {
+        name: "mysql",
+        link: "https://www.mysql.com/",
+      },
+      {
+        name: "javascript",
+        link: "https://www.javascript.com/",
+      },
+    ],
+    visit: "https://scalabilityengineers.com/",
     id: 2,
   },
+
   {
-    title: 'SR7',
+    title: "SR7",
     description: " An E-commerce site for a clothing brand named SR7.",
-    image: '/images/sr7.png',
-    tags: ['Javascript', 'Php', 'MySQL'],
-    source: 'http://swetharaju7.com/',
-    visit: 'http://swetharaju7.com/',
+    image: "/images/sr7.png",
+    tags: [
+      {
+        name: "php",
+        link: "https://www.php.net/",
+      },
+      {
+        name: "mysql",
+        link: "https://www.mysql.com/",
+      },
+      {
+        name: "javascript",
+        link: "https://www.javascript.com/",
+      },
+      {
+        name: "php Mailer",
+        link: "https://github.com/PHPMailer/PHPMailer",
+      },
+    ],
+    visit: "http://swetharaju7.com/",
     id: 3,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2018, text: 'Started my journey', },
-  { year: 2019, text: 'Learnt web development with python and php', },
-  { year: 2020, text: 'Started working as a freelance developer with my first E-commerce', },
-  { year: 2021, text: 'Switched to MERN Stack. Landed on a MERN stack developer job at Sierra digital inc.', },
+  { year: 2018, text: "Started my journey" },
+  { year: 2019, text: "Learnt web development with python and php" },
+  {
+    year: 2020,
+    text: "Started working as a freelance developer with my first E-commerce",
+  },
+  {
+    year: 2021,
+    text: "Switched to MERN Stack. Landed on a MERN stack developer job at Sierra digital inc.",
+  },
+];
+
+export const MyProjects = [
+  {
+    title: "Crypton",
+    description:
+      "This a react application which let's user to browse details about cryptocurriencies, latest cryptocurrency news and details on exchanges. I've built this project to get a clear understanding of how redux and RTK Queries work. This project is built on top of more than 3 APIs which are provided by RapidApi. The UI is done with Ant Design and charts are plotted with Recharts",
+    image: "/images/crypton.png",
+    source: "https://github.com/alphawhiskey03/crypto-app",
+    visit: "https://crypton03.netlify.app",
+    tags: [
+      {
+        name: "react",
+        link: "https://reactjs.org/",
+      },
+      {
+        name: "redux toolkit",
+        link: "https://redux-toolkit.js.org/",
+      },
+      {
+        name: "ant design",
+        link: "https://ant.design/",
+      },
+      {
+        name: "recharts",
+        link: "https://recharts.org/en-US/",
+      },
+    ],
+  },
+  {
+    title: "RICKED",
+    description:
+      "An app mimicing a social media platform , The app lets user register an account and the user can post pictures, content,comment on posts ,like posts etc ... React context is used for centralizing application state. Apollo client is used as the grahql client in the app. it's user interface is completely designed with MUI5 . For the file storage firebase storage has been used, User authentication is managed with jsonwebtoken . The database used is MongoDB with mongoose as ODM (Object data model). The data is served with Apollo-server which is one of the commonly used GraphQL server.",
+    image: "/images/ricked.png",
+    source: "https://github.com/alphawhiskey03/social-media-app",
+    visit: "https://ricked.netlify.app/",
+    tags: [
+      {
+        name: "react",
+        link: "https://reactjs.org/",
+      },
+      {
+        name: "graphql",
+        link: "https://graphql.org/",
+      },
+      {
+        name: "apollo",
+        link: "https://www.apollographql.com/",
+      },
+      {
+        name: "mui",
+        link: "https://mui.com/",
+      },
+
+      {
+        name: "react context",
+        link: "https://reactjs.org/docs/context.html",
+      },
+      {
+        name: "firebase",
+        link: "https://firebase.google.com/",
+      },
+      {
+        name: "jsonwebtokens",
+        link: "https://jwt.io/",
+      },
+    ],
+  },
+  {
+    title: "My portfolio website",
+    description:
+      "In the process of learning next.js, i built my portfolio website along with styled components",
+    image: "/images/portfolio.png",
+    source: "https://github.com/alphawhiskey03/portfolio-site",
+    tags: [
+      {
+        name: "next.js",
+        link: "https://nextjs.org/",
+      },
+      {
+        name: "styled components",
+        link: "https://styled-components.com/",
+      },
+    ],
+  },
 ];
