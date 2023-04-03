b2b915eee2a3086f1d798459abf0d96a951b2dd8
vignesh <vignesh@vigneshs-Air.domain.name>
2023-04-04 00:07:50 +0530

    feat(client): loading custom app component with initial props

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,31 @@
+import App from "next/app";
 import Theme from "../styles/theme";
+import { client } from "../sanity";
+import { GET_MAINCONTENT, GET_SOCIALMEDIA } from "../constants";
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({
+  Component,
+  pageProps,
+  mainContent,
+  socialMedia,
+}) {
   return (
     <>
       <Theme>
-        <Component {...pageProps} />
+        <Component
+          {...pageProps}
+          mainContent={mainContent}
+          socialMedia={socialMedia}
+        />
       </Theme>
     </>
   );
 }
+
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  const mainContent = await client.fetch(GET_MAINCONTENT);
+  const socialMedia = await client.fetch(GET_SOCIALMEDIA);
+
+  return { ...appProps, mainContent, socialMedia };
+};
