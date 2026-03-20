e6743969924b3ab896c0c26eb18c357b3357f84b
alphawhiskey03 <vignesh.shanmugavelu@getbeamer.com>
2026-03-20 15:33:45 +0530

    feat: switch to react-icons instead of lucide-react for social links

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,5 +1,5 @@
-import { LiquidMetalButton } from "@/components/ui/liquid-metal-button";
-import { Github, Linkedin, X, Globe } from "lucide-react";
+import { LiquidMetalButton } from "@/components/jolly-ui/liquid-metal-button";
+import { BsTwitterX, BsGithub, BsLinkedin, BsGlobe } from "react-icons/bs";
 
 export enum LinkType {
   GITHUB = "github",
@@ -10,41 +10,51 @@ export enum LinkType {
 
 const TypeToIcon: Record<
   LinkType,
-  { icon: React.ElementType; text: string | null; href: string }
+  { icon: React.ElementType; text?: string; href: string }
 > = {
   [LinkType.GITHUB]: {
-    icon: Github,
+    icon: BsGithub,
     text: "Github",
     href: "https://github.com/alphawhiskey03",
   },
   [LinkType.LINKEDIN]: {
-    icon: Linkedin,
+    icon: BsLinkedin,
     text: "LinkedIn",
     href: "https://www.linkedin.com/in/vignesh-s-802812183/",
   },
   [LinkType.X]: {
-    icon: X,
-    text: null,
+    icon: BsTwitterX,
+    text: undefined,
     href: "https://x.com/Vignesh03_",
   },
   [LinkType.PORTFOLIO]: {
-    icon: Globe,
+    icon: BsGlobe,
     text: "Portfolio",
     href: "https://therealvig.netlify.app",
   },
 };
 
-export function LinkPageButtons({ type }: { type: LinkType }) {
+interface LinkButtonProps {
+  type: LinkType;
+  viewMode?: "text" | "icon";
+}
+
+export const LinkButton: React.FC<LinkButtonProps> = ({
+  type,
+  viewMode = "icon",
+}) => {
   const { icon: Icon, text, href } = TypeToIcon[type];
+
   const onClick = () => {
     window.open(href, "_blank");
   };
   return (
     <LiquidMetalButton
       size="md"
+      viewMode={viewMode}
       icon={<Icon size={16} className="text-white" />}
       label={text}
       onClick={onClick}
     />
   );
-}
+};
